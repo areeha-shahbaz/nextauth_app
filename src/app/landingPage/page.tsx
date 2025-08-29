a3e25d423b1a562057f47063b88b418afb8e9c86
areeha <ranaareeha62@gmail.com>
2025-08-29 11:46:45 +0500

    Remove landingPage page.tsx and set LandingPage as root

--- src/app/landingPage/page.tsx
+++
@@ -1,9 +0,0 @@
-"use client";
-
-import dynamic from "next/dynamic";
-
-const LandingPage = dynamic(() => import("./LandingPage"), { ssr: false });
-
-export default function Page() {
-  return <LandingPage />;
-}
