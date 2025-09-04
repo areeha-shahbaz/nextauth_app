ea15a13bf5494df08a58629283a897d46b98892f
areeha <ranaareeha62@gmail.com>
2025-09-04 15:51:04 +0500

    change the reset page

--- src/app/non-auth/reset-password/page.tsx
+++ src/app/non-auth/reset-password/page.tsx
@@ -1,5 +1,20 @@
-import ResetPasswordClient from "./ResetPasswordClient";
+// import ResetPasswordClient from "./ResetPasswordClient";
 
-export default function ResetPasswordPage() {
-  return <ResetPasswordClient />;
+// export default function ResetPasswordPage() {
+//   return <ResetPasswordClient />;
+// }
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
+
+const ResetPasswordClient = dynamic(
+  () => import("./ResetPasswordClient"),
+  { ssr: false } 
+);
+
+export default function Page() {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <ResetPasswordClient />
+    </Suspense>
+  );
 }
