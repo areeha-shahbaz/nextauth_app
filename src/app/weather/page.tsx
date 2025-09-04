c44d209d682d4c9b45f89cb679b982d63115c38f
areeha <ranaareeha62@gmail.com>
2025-09-04 15:48:16 +0500

    made the coords changes

--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -3,7 +3,6 @@ import { useState } from "react";
 import WeatherMap from "src/app/components/WeatherMap";
 
 export default function WeatherPage() {
-  const [coords, setCoords] = useState<[number, number] | null>(null);
 
   return <WeatherMap />;
 }
