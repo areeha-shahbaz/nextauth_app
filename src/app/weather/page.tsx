5185ed09ff208ddefd3edbe6712bfd8bd896cc92
areeha <ranaareeha62@gmail.com>
2025-09-05 11:45:35 +0500

    made change in the route

--- src/app/weather/page.tsx
+++ src/app/weather/page.tsx
@@ -1,16 +1,18 @@
-"use client";
-import WeatherMap from "src/app/components/WeatherMap";
-
-export default function WeatherPage() {
-
-  return <WeatherMap />;
-}
-// import dynamic from "next/dynamic";
-
-// const WeatherMap = dynamic(() => import("src/app/components/WeatherMap"), {
-//   ssr: false,
-// });
+// "use client";
+// import WeatherMap from "src/app/components/WeatherMap";
 
 // export default function WeatherPage() {
+
 //   return <WeatherMap />;
 // }
+"use client";
+import dynamic from "next/dynamic";
+
+const WeatherMap = dynamic(() => import("src/app/components/WeatherMap"), {
+  ssr: false, 
+});
+
+export default function WeatherPage() {
+  return <WeatherMap />;
+}
+
