d69f0db14a756ed378632f6ee29ab79ae828ced3
areeha <ranaareeha62@gmail.com>
2025-09-18 00:27:34 +0500

    fix error

--- src/app/auth/image/page.tsx
+++ src/app/auth/image/page.tsx
@@ -1,10 +1,12 @@
 "use client";
 import dynamic from "next/dynamic";
 
+import { useRequireAuth } from "../../../authCondition";
 const ImageAnalyzer = dynamic(() => import("src/app/components/image-analyzer"), {
   ssr: false, 
 });
 
 export default function ImagePage() {
+  useRequireAuth();
   return <ImageAnalyzer/>;
 }
