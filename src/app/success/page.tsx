1e05b6d06f2ef5e8e9fc5cad8bb2b6501ee5d476
areeha <ranaareeha62@gmail.com>
2025-10-11 13:31:07 +0500

    changed the a tag

--- src/app/success/page.tsx
+++ src/app/success/page.tsx
@@ -1,21 +1,77 @@
+// "use client";
+// import { useEffect } from "react";
+// import { useCart } from "src/app/context/CartContext";
+// export default function Success() {
+//     const { clearCart } = useCart();
+
+//   useEffect(() => {
+//     clearCart(); 
+//   }, [clearCart]);
+
+//   return (
+//     <div style ={{backgroundColor:"#ffffffff", height:"180px",width:"300px",borderRadius:"10px",boxShadow:"rgba(0, 0, 0, 0.56) 0px 22px 70px 4px" }}>
+//     <div style={{ textAlign: "center" ,marginTop: "3rem" }}>
+//       <h2 style={{color:"darkgreen",fontSize:"25px"}}>Payment Successful!</h2>
+//       <br></br>
+//       <p>Thank you for your purchase.</p>
+//       <a href="/auth/fakeStore" style={{fontStyle:"italic", fontSize:"14px"}}>Continue Shopping</a>
+//     </div>
+//     </div>
+//   );
+// }
+
+
+
 "use client";
+
 import { useEffect } from "react";
+import Link from "next/link";
 import { useCart } from "src/app/context/CartContext";
+
 export default function Success() {
-    const { clearCart } = useCart();
+  const { clearCart } = useCart();
 
   useEffect(() => {
-    clearCart(); 
+    clearCart();
   }, [clearCart]);
 
   return (
-    <div style ={{backgroundColor:"#ffffffff", height:"180px",width:"300px",borderRadius:"10px",boxShadow:"rgba(0, 0, 0, 0.56) 0px 22px 70px 4px" }}>
-    <div style={{ textAlign: "center" ,marginTop: "3rem" }}>
-      <h2 style={{color:"darkgreen",fontSize:"25px"}}>Payment Successful!</h2>
-      <br></br>
-      <p>Thank you for your purchase.</p>
-      <a href="/auth/fakeStore" style={{fontStyle:"italic", fontSize:"14px"}}>Continue Shopping</a>
-    </div>
+    <div
+      style={{
+        backgroundColor: "#ffffffff",
+        height: "180px",
+        width: "300px",
+        borderRadius: "10px",
+        boxShadow: "rgba(0, 0, 0, 0.56) 0px 22px 70px 4px",
+        margin: "auto",
+        marginTop: "8rem",
+        padding: "20px",
+        textAlign: "center",
+      }}
+    >
+      <h2 style={{ color: "darkgreen", fontSize: "25px" }}>
+        Payment Successful!
+      </h2>
+      <p style={{ marginTop: "10px", marginBottom: "20px" }}>
+        Thank you for your purchase.
+      </p>
+
+      <Link
+        href="/auth/fakeStore"
+        style={{
+          display: "inline-block",
+          padding: "8px 16px",
+          backgroundColor: "teal",
+          color: "white",
+          borderRadius: "8px",
+          fontStyle: "italic",
+          fontSize: "14px",
+          textDecoration: "none",
+          transition: "background 0.3s",
+        }}
+      >
+        Continue Shopping
+      </Link>
     </div>
   );
 }
